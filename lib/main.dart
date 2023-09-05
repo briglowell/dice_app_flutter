80faa0014d70700cdf67181acf5e75cab13381e5
babajaeger <brig.lowell@gmail.com>
2023-09-05 14:43:03 -0600

    built simple random dice_app in flutter

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:dice_app/gradient_container.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Scaffold(body: GradientContainer.purple()),
+    ),
+  );
+}
